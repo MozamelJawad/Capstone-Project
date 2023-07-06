c8cffecb4dffd2e1b7c0a743d998d0aa5ee81717
Mozamel Jawad <mozameljawad@gmail.com>
2023-07-06 18:39:04 +0430

    linters errors solved

--- script.js
+++ script.js
@@ -1,55 +1,55 @@
 const speakers = [
-    {
-        id: 1,
-        speakerName: 'Mozamel Jawad',
-        speakerPosition: 'Database and Software Developer',
-        imgSource:'assets/speakers/1.jpg',
-        description: 'He has Studied Computer Science Faculty at Kabul University, and currently working as a Senior Database and Software Developer with ABC Company',
-    },
-    {
-        id: 2,
-        speakerName: 'Modasir Jawad',
-        speakerPosition: 'Database General Manager',
-        imgSource:'assets/speakers/2.jpg',
-        description: 'He has Studied Computer Science Faculty at Nangrahar University, and currently working as a Database and Software Manager with CBA Company',
-    },
-    {
-        id: 3,
-        speakerName: 'M.Mozamel Jawad',
-        speakerPosition: 'Full Stack Developer',
-        imgSource:'assets/speakers/3.jpg',
-        description: 'He has Studied Computer Science at Kabul University, and currently working as a Senior Database and Software Developer with ABC Company',
-    },
-    {
-        id: 4,
-        speakerName: 'Ahmad Jawad',
-        speakerPosition: 'Database and Software Developer',
-        imgSource:'assets/speakers/1.jpg',
-        description: 'He has Studied Computer Science Faculty at Kabul University, and currently working as a Senior Database and Software Developer with ABC Company',
-    },
-    {
-        id: 5,
-        speakerName: 'M. Modasir Jawad',
-        speakerPosition: 'Database General Manager',
-        imgSource:'assets/speakers/2.jpg',
-        description: 'He has Studied Computer Science Faculty at Nangrahar University, and currently working as a  Database and Software Manager with CBA Company',
-    },
-    {
-        id: 6,
-        speakerName: 'Jawad Mozamel',
-        speakerPosition: 'Full Stack Developer',
-        imgSource:'assets/speakers/3.jpg',
-        description: 'He has Studied Computer Science at Kabul University, and currently working as a Senior Database and Software Developer with ABC Company',
-    },
-   
+  {
+    id: 1,
+    speakerName: 'Mozamel Jawad',
+    speakerPosition: 'Database and Software Developer',
+    imgSource: 'assets/speakers/1.jpg',
+    description: 'He has Studied Computer Science Faculty at Kabul University, and currently working as a Senior Database and Software Developer with ABC Company',
+  },
+  {
+    id: 2,
+    speakerName: 'Modasir Jawad',
+    speakerPosition: 'Database General Manager',
+    imgSource: 'assets/speakers/2.jpg',
+    description: 'He has Studied Computer Science Faculty at Nangrahar University, and currently working as a Database and Software Manager with CBA Company',
+  },
+  {
+    id: 3,
+    speakerName: 'M.Mozamel Jawad',
+    speakerPosition: 'Full Stack Developer',
+    imgSource: 'assets/speakers/3.jpg',
+    description: 'He has Studied Computer Science at Kabul University, and currently working as a Senior Database and Software Developer with ABC Company',
+  },
+  {
+    id: 4,
+    speakerName: 'Ahmad Jawad',
+    speakerPosition: 'Database and Software Developer',
+    imgSource: 'assets/speakers/1.jpg',
+    description: 'He has Studied Computer Science Faculty at Kabul University, and currently working as a Senior Database and Software Developer with ABC Company',
+  },
+  {
+    id: 5,
+    speakerName: 'M. Modasir Jawad',
+    speakerPosition: 'Database General Manager',
+    imgSource: 'assets/speakers/2.jpg',
+    description: 'He has Studied Computer Science Faculty at Nangrahar University, and currently working as a  Database and Software Manager with CBA Company',
+  },
+  {
+    id: 6,
+    speakerName: 'Jawad Mozamel',
+    speakerPosition: 'Full Stack Developer',
+    imgSource: 'assets/speakers/3.jpg',
+    description: 'He has Studied Computer Science at Kabul University, and currently working as a Senior Database and Software Developer with ABC Company',
+  },
+
 ];
 
 speakers.forEach((speaker) => {
-    const speakerSection = document.getElementById('mainSection');
-    const partDiv = document.createElement('div');
-    partDiv.className = 'col-md-5';
-    partDiv.className = 'speaker-card';
-    partDiv.innerHTML = `
+  const speakerSection = document.getElementById('mainSection');
+  const partDiv = document.createElement('div');
+  partDiv.className = 'col-md-5';
+  partDiv.className = 'speaker-card';
+  partDiv.innerHTML = `
         <div class="speaker">
                 <img src="${speaker.imgSource}" alt="speaker image" class="img-fluid speaker-photo">
             </div> 
@@ -60,21 +60,25 @@ speakers.forEach((speaker) => {
                 <p class="speaker-exp">${speaker.description}</p>
             </div>`;
 
-    speakerSection.append(partDiv);
+  speakerSection.append(partDiv);
 });
 
 function ShowXicon() {
-    const iconMenu = document.getElementById('meneIcon');
-    const iconX = document.getElementById('Xicon');
-    
-    iconX.style.display = 'inline';
-    iconMenu.style.display = 'none';
+  const iconMenu = document.getElementById('meneIcon');
+  const iconX = document.getElementById('Xicon');
+
+  iconX.style.display = 'inline';
+  iconMenu.style.display = 'none';
 }
+
+ShowXicon();
 
 function removeXiocn() {
-    const iconMenu = document.getElementById('meneIcon');
-    const iconX = document.getElementById('Xicon');
+  const iconMenu = document.getElementById('meneIcon');
+  const iconX = document.getElementById('Xicon');
 
-    iconX.style.display = 'none';
-    iconMenu.style.display = 'block';
+  iconX.style.display = 'none';
+  iconMenu.style.display = 'block';
 }
+
+removeXiocn();
